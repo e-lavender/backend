337f450bdbb3704a3569b9f11ee053e1ab45769d
alexanderbalaban24 <alexanderbalaban24@gmail.com>
2023-10-31 01:35:00 +0300

    chore: Configured and create base architecture

--- apps/inctagram-backend/src/main.ts
+++ apps/inctagram-backend/src/main.ts
@@ -1,8 +1,36 @@
+import { GlobalConfigService } from './config/config.service';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { useContainer } from 'class-validator';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
+import {
+  ErrorExceptionFilter,
+  HttpExceptionFilter,
+} from '../../../libs/filters/exception.filter';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(GlobalConfigService);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidUnknownValues: false,
+      exceptionFactory: (errors) => {
+        const customErrors = errors.map((err) => {
+          return {
+            field: err.property,
+            messages: Object.values(err.constraints),
+          };
+        });
+        throw new BadRequestException(customErrors);
+      },
+    }),
+  );
+  app.useGlobalFilters(new ErrorExceptionFilter(), new HttpExceptionFilter());
+  app.enableCors();
+  app.use(cookieParser());
+
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+  await app.listen(configService.getPort('users'));
 }
 bootstrap();
