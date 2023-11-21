0085ae3c9550c5d2166ec8171c21a30b62f4aa5e
alexanderbalaban24 <alexanderbalaban24@gmail.com>
2023-11-21 18:14:25 +0300

    fix: fix swagger

--- apps/inctagram-backend/src/main.ts
+++ apps/inctagram-backend/src/main.ts
@@ -40,7 +40,9 @@ async function bootstrap() {
     .addTag('Auth')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('/swagger', app, document);
+  SwaggerModule.setup('/swagger', app, document, {
+    swaggerOptions: { defaultModelsExpandDepth: -1 },
+  });
 
   const port = configService.getPort('users');
   await app.listen(port);
