8fc4a297d94e08cf6ac9c751eff5f8e2593eb6bc
Artyom Golubev <artgolubev@bk.ru>
2024-01-07 11:05:58 +0300

    !!!: most of this changes unknown

---
+++ apps/inctagram-backend/config/config.module.ts
@@ -0,0 +1,21 @@
+import { ConfigModule } from '@nestjs/config';
+import { getConfiguration } from './configuration';
+import * as Joi from 'joi';
+
+export const configModule = ConfigModule.forRoot({
+  isGlobal: true,
+  load: [getConfiguration],
+  validationSchema: Joi.object({
+    USERS_SERVICES_PORT: Joi.number().required(),
+    SALT_GENERATE_ROUND: Joi.number().required(),
+    GMAIL: Joi.string().required(),
+    GMAIL_PASS: Joi.string().required(),
+    DOMAIN: Joi.string().required(),
+    FRONT_DOMAIN: Joi.string().required(),
+    JWT_SECRET: Joi.string().required(),
+    POSTGRES_DIRECT_URL: Joi.string().required(),
+    POSTGRES_URL: Joi.string().required(),
+    FILE_SERVICE_HOST: Joi.string().required(),
+    FILE_SERVICE_PORT: Joi.number().required(),
+  }),
+});
